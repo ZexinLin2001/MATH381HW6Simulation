2862bb56ca0b2cb002d792dede5d79c838a2ef1d
zexinlin <linzexin@uw.edu>
2022-11-14 16:17:30 -0800

    merge repo

--- dataProcessing/progression.py
+++ dataProcessing/progression.py
@@ -10,33 +10,27 @@ def mean_confidence_interval(data, confidence=0.95):
     return m, m-h, m+h
 
 
-num = 1
+num = 3
 
-arr = np.loadtxt("data/single-s" + str(num) + ".csv", delimiter=",", dtype='int16')
+arr = np.loadtxt("../data/single-s" + str(num) + ".csv", delimiter=",", dtype='int16')
 
 means = []
-ci95 = []
-ci99 = []
-for i in range(10, len(arr)//100 + 1, 10):
-    mean, ci95l, ci95h = mean_confidence_interval(arr[0:i])
-    mean, ci99l, ci99h = mean_confidence_interval(arr[0:i], 0.99)
-    means.append(mean)
-    ci95.append([ci95l, ci95h])
-    ci99.append([ci99l, ci99h])
 
-fig, ax = plt.plot([i for i in range(10, len(arr)//100 + 1, 10)], ci95, color='b')
-plt.plot([i for i in range(10, len(arr)//100 + 1, 10)], ci99, color='r')
-plt.plot([i for i in range(10, len(arr)//100 + 1, 10)], means, color='k')
+for i in range(10):
+    temp = []
+    for j in range(1000):
+        temp.append(np.mean(arr[10000 * i: 10000 * i + j + 1]))
+    means.append(temp)
+
+means = np.array(means)
+means = np.transpose(means)
+
+plt.figure(figsize=(9, 6), dpi=100)
+_ = plt.plot([i for i in range(1000)],means, color='k')
+
 
 plt.xlabel("Number of games")
-plt.ylabel("Value of ...")
-plt.legend(["95CI low", "95CI high", "99CI low", "99CI high", "mean"])
-plt.title("Single Player with Strategy " + str(num) + ", first 10^4 of 10^6 simulations")
+plt.ylabel("Expectation Game Length")
+plt.title("Expectation of Single Player with Strategy " + str(num) + " of 10 runs of 1000 simulations")
 
 plt.show()
-
-print(scipy.stats.describe(arr))
-
-# s1: DescribeResult(nobs=1000000, minmax=(26, 157), mean=64.477177, variance=151.03100014167106, skewness=0.6474048123612509, kurtosis=0.7619243597145222)
-# s2: DescribeResult(nobs=1000000, minmax=(23, 149), mean=63.696428, variance=142.2508562916724, skewness=0.645568122548458, kurtosis=0.768070487884335)
-# s3: DescribeResult(nobs=1000000, minmax=(21, 147), mean=50.550289, variance=98.39289140937043, skewness=0.8317789353822028, kurtosis=1.385522062944795)
